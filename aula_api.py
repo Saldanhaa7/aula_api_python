9391a0b56bd00435180804973f88b3bdc5f72c6b
saldanhaa7 <bruno14042003@gmail.com>
2022-09-11 20:46:14 -0300

    api_python

---
+++ aula_api.py
@@ -0,0 +1,19 @@
+from errno import ESTALE
+import requests
+import json
+
+cotacao = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+cotacao = cotacao.json()
+cotacao_dolar = float(cotacao['USDBRL'] ['bid'])
+valor = int(input('valor em dólar '))
+valor_total = valor*cotacao_dolar
+#print(type(cotacao_dolar))
+#print(type(valor))
+#print(valor_total)
+#print(cotacao)
+if valor == 0:
+    print('valor inválido, por favor digite outro')
+if valor == 1:
+    print(str(valor) + ' dólar vale ' + str(cotacao_dolar) + ' reais')
+if valor > 1:
+    print(str(valor) + ' dólares vale ' + str(valor_total) + ' reais')
